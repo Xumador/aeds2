db4ddddb73fafa072438e469141038a8c8bd777f
José Reis <84919858+Xumador@users.noreply.github.com>
2022-09-27 11:53:03 -0300

    Update ordenacao.cpp

--- ordenacao.cpp
+++ ordenacao.cpp
@@ -5,7 +5,8 @@ void imprimevetor(int *v)
 {
     for (int *p = v; p < v + 10; p++)
         cout << " " << *p;
-    cout << endl;
+    cout << endl
+         << endl;
 }
 void bubblesort(int *v)
 {
@@ -21,6 +22,36 @@ void bubblesort(int *v)
                 v[j + 1] = aux;
             }
         }
+    }
+}
+void insertionsort(int *v)
+{
+    int aux, j;
+    for (int i = 1; i < 10; i++)
+    {
+        aux = v[i];
+        j = i - 1;
+        while (j >= 0 && v[j] > aux)
+        {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = aux;
+    }
+}
+void pointerinsertionsort(int *v)
+{
+    int aux, *j;
+    for (int *p = v + 1; p < v + 10; p++)
+    {
+        aux = *p;
+        j = p-1;
+        while (j >= v && *j > aux)
+        {
+            *(j + 1) = *j;
+            j--;
+        }
+        *(j + 1) = aux;
     }
 }
 void pointerbubblesort(int *v)
@@ -47,13 +78,38 @@ int main(int argc, char **argv)
     int vb0[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int vb1[10] = {5, 8, 2, 0, 6, 9, 1, 4, 3, 7};
     int vb2[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-
+    int vb3[10] = {6, 8, 3, 9, 5, 4, 7, 2, 1, 0};
+    cout << "Bubble Sort, vetor ordenado: " << endl;
+    imprimevetor(vb0);
+    bubblesort(vb0);
+    imprimevetor(vb0);
+    cout << "Pointer Bubble Sort, vetor ordenado: " << endl;
+    imprimevetor(vb0);
+    pointerbubblesort(vb0);
+    imprimevetor(vb0);
     cout << "Bubble Sort, vetor ordenado: " << endl;
     imprimevetor(vb1);
     bubblesort(vb1);
     imprimevetor(vb1);
     cout << "Pointer Bubble Sort, vetor ordenado: " << endl;
+    imprimevetor(vb1);
+    pointerbubblesort(vb1);
+    imprimevetor(vb1);
+    cout << "Bubble Sort, vetor ordenado: " << endl;
+    imprimevetor(vb2);
+    bubblesort(vb2);
+    imprimevetor(vb2);
+    cout << "Pointer Bubble Sort, vetor ordenado: " << endl;
     imprimevetor(vb2);
     pointerbubblesort(vb2);
     imprimevetor(vb2);
+     cout << "Insertion Sort, vetor ordenado: " << endl;
+    imprimevetor(vb3);
+    //insertionsort(vb3);
+    imprimevetor(vb3);
+    cout << "Pointer Insertion Sort, vetor ordenado: " << endl;
+    imprimevetor(vb3);
+    pointerinsertionsort(vb3);
+    imprimevetor(vb3);
+    // FALTA IMPLEMENTAR OS OUTROS MÉTODOS DE ORDENAÇÃO
 }
