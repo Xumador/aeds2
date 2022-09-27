cdd4de6bb31433e621f67c539a3d9c10283d1ff9
José Reis <84919858+Xumador@users.noreply.github.com>
2022-09-27 11:29:35 -0300

    Create troca.cpp

---
+++ troca.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int troca(int a, int b)
+{
+    int aux;
+    aux = a;
+    a = b;
+    b = aux;
+}
+void troca2(int *a, int *b)
+{
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
+int main()
+{
+    int x = 11, y = 22;
+    //cout << troca(x, y) << endl;
+    troca2(&x, &y);
+    cout << x << endl
+         << y << endl;
+    return 0;
+}
